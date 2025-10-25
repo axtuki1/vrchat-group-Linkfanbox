036dbf65036d69f1d96e1ac7283609a3c00e8f3f
axtuki1 <master@axtuki1.com>
2025-10-26 03:50:15 +0900

    update 例の文字列を入れ忘れてた

--- src/discord/client/messageEvents/pt_BABEL.ts
+++ src/discord/client/messageEvents/pt_BABEL.ts
@@ -21,6 +21,20 @@ export class BABELMessageEvent extends MessageEventHandler {
         if (event.message.content !== this.triggerWord) {
             return;
         }
+        let msgBody = "Go to, let us go down, "
+        const mes = await event.message.author.send(msgBody);
+        await sleep(2000);
+        msgBody += "and there confound their language, ";
+        await mes.edit(msgBody);
+        await sleep(2000);
+        msgBody += "\nthat they may not understand";
+        await mes.edit(msgBody);
+        await sleep(2000);
+        msgBody += "one another's speech.";
+        await mes.edit(msgBody);
+        await sleep(5000);
+        await mes.delete();
+        await sleep(1000);
         const msgs = [];
         for (let i = 0; i < this.count; i++) {
             msgs.push(await event.message.author.send(this.babel));
